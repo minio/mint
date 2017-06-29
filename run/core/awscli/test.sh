9cb890cd36d44f8afdc63a3d27aa9e33a3797941
Harshavardhana <harsha@minio.io>
2017-06-29 15:13:34 -0700

    Add awscli tests (#51)
    - Added listObjects v1 and v2
    - Added multipart (create, upload and complete multipart)
    - Adds negative cases as well for list-objects, putobject
    - Adds a case for uploading a large 65MB file.
    - Adds a case for recursively uploading a directory.

--- run/core/awscli/test.sh
+++ run/core/awscli/test.sh
@@ -26,7 +26,7 @@ create_random_string() {
 
 createBucket_01(){
     local bucketName
-    
+
     bucketName=$(create_random_string)
 
     echo "Running createBucket_01"
@@ -66,7 +66,7 @@ createObject_02(){
     hash2=$(md5sum "/tmp/datafile-1-MB-downloaded" | awk '{print $1}')
 
     echo "Testing if the downloaded file is same as local file"
-    if [ "$hash1" -ne "$hash2" ]; then
+    if [ "$hash1" != "$hash2" ]; then
         return 1
     fi
 
@@ -77,10 +77,242 @@ createObject_02(){
     ${AWS} s3api delete-bucket --bucket "${bucketName}"
 }
 
+listObjects_03() {
+    echo "Running listObjects_03"
+    local bucketName
+    local fileName
+    local baseFileName
+
+    bucketName=$(create_random_string)
+
+    # create a bucket
+    echo "Create a bucket"
+    ${AWS} s3api create-bucket --bucket "${bucketName}"
+
+    fileName="${DATA_DIR}/datafile-1-MB"
+    baseFileName="$(basename "${fileName}")"
+
+    # upload a file
+    echo "Upload an object"
+    ${AWS} s3api put-object --body "${fileName}" --bucket "${bucketName}" --key "${baseFileName}"
+
+    keyName=$(${AWS} s3api list-objects --bucket "${bucketName}" --prefix "${baseFileName}" | jq -r .Contents[].Key)
+    if [ "$keyName" != "datafile-1-MB" ]; then
+        echo "Unexpected $keyName, expecting datafile-1-MB"
+        return 1
+    fi
+
+    keyName=$(${AWS} s3api list-objects --bucket "${bucketName}" --prefix "linux" | jq -r .Contents[].Key)
+    if [ "$keyName" != "" ]; then
+        echo "Unexpected $keyName found, expecting empty key"
+        return 1
+    fi
+
+    keyName=$(${AWS} s3api list-objects-v2 --bucket "${bucketName}" --prefix "${baseFileName}" | jq -r .Contents[].Key)
+    if [ "$keyName" != "datafile-1-MB" ]; then
+        echo "Unexpected $keyName, expecting datafile-1-MB"
+        return 1
+    fi
+
+    keyName=$(${AWS} s3api list-objects-v2 --bucket "${bucketName}" --prefix "linux" | jq -r .Contents[].Key)
+    if [ "$keyName" != "" ]; then
+        echo "Unexpected $keyName found, expecting empty key"
+        return 1
+    fi
+
+    echo "Remove the object"
+    ${AWS} s3api delete-object --bucket "${bucketName}" --key "${baseFileName}"
+
+    echo "Remove the bucket"
+    ${AWS} s3api delete-bucket --bucket "${bucketName}"
+}
+
+multipart_04() {
+    echo "Running multipart_04"
+    local bucketName
+    local fileName1
+    local fileName2
+
+    bucketName=$(create_random_string)
+    objectName=$(create_random_string)
+    fileName1="${DATA_DIR}/datafile-5-MB"
+    fileName2="${DATA_DIR}/datafile-1-MB"
+
+    # create a bucket
+    echo "Create a bucket"
+    ${AWS} s3api create-bucket --bucket "${bucketName}"
+
+    # create multipart
+    uploadID=$(${AWS} s3api create-multipart-upload --bucket "${bucketName}" --key "${objectName}" | jq -r .UploadId)
+
+    # Capture etag for part-number 1
+    etag1=$(${AWS} s3api upload-part --bucket "${bucketName}" --key "${objectName}" --body "${fileName1}" --upload-id "${uploadID}" --part-number 1 | jq -r .ETag)
+
+    # Capture etag for part-number 2
+    etag2=$(${AWS} s3api upload-part --bucket "${bucketName}" --key "${objectName}" --body "${fileName2}" --upload-id "${uploadID}" --part-number 2 | jq -r .ETag)
+
+    # Create a multipart struct file for completing multipart transaction
+    echo "{
+        \"Parts\": [
+            {
+                \"ETag\": ${etag1},
+                \"PartNumber\": 1
+            },
+            {
+                \"ETag\": ${etag2},
+                \"PartNumber\": 2
+            }
+        ]
+    }" >> /tmp/multipart
+
+    # Use saved etags to complete the multipart transaction
+    finalETag=$(${AWS} s3api complete-multipart-upload --multipart-upload file:///tmp/multipart --bucket "${bucketName}" --key "${objectName}" --upload-id "${uploadID}" | jq -r .ETag)
+    if [ "${finalETag}" == "" ]; then
+        echo "Unexpected empty etag, expecting a non etag"
+        return 1
+    fi
+
+    echo "Remove the object"
+    ${AWS} s3api delete-object --bucket "${bucketName}" --key "${objectName}"
+
+    echo "Remove the bucket"
+    ${AWS} s3api delete-bucket --bucket "${bucketName}"
+}
+
+copy_05() {
+    local bucketName
+    local objectName
+
+    bucketName=$(create_random_string)
+    objectName=$(create_random_string)
+    fileName="${DATA_DIR}/datafile-65-MB"
+
+    echo "Running copy_05"
+    # Create a bucket.
+    ${AWS} s3api create-bucket --bucket "${bucketName}"
+
+    echo "Upload a large file"
+    ${AWS} s3 cp "$fileName" "s3://${bucketName}/$(basename "$fileName")"
+
+    echo "Remove an object"
+    ${AWS} s3 rm "s3://${bucketName}/$(basename "$fileName")"
+
+    echo "Remove bucket"
+    ${AWS} s3 rb "s3://${bucketName}/"
+}
+
+sync_06() {
+    local bucketName
+    local objectName
+
+    bucketName=$(create_random_string)
+    objectName=$(create_random_string)
+    data_dir="${DATA_DIR}"
+
+    echo "Running sync_06"
+    # Create a bucket.
+    ${AWS} s3api create-bucket --bucket "${bucketName}"
+
+    echo "Upload a set of files"
+    ${AWS} s3 sync "$data_dir" "s3://${bucketName}/"
+
+    echo "Remove bucket contents recursively"
+    ${AWS} s3 rm --recursive "s3://${bucketName}/"
+
+    echo "Remove the bucket"
+    ${AWS} s3api delete-bucket --bucket "${bucketName}"
+}
+
+listObjects_error_01() {
+    echo "Running listObjects_03"
+    local bucketName
+    local fileName
+    local baseFileName
+
+    bucketName=$(create_random_string)
+    fileName="${DATA_DIR}/datafile-1-MB"
+    baseFileName="$(basename "${fileName}")"
+
+    # create a bucket
+    echo "Create a bucket"
+    ${AWS} s3api create-bucket --bucket "${bucketName}"
+
+    # upload a file
+    echo "Upload an object"
+    ${AWS} s3api put-object --body "${fileName}" --bucket "${bucketName}" --key "${baseFileName}"
+
+    # Server replies an error for v1 with max-key=-1
+    ${AWS} s3api list-objects --bucket "${bucketName}" --prefix "${baseFileName}" --max-keys=-1
+    if [ $? -ne 255 ]; then
+        return 1
+    fi
+
+    # Server replies an error for v2 with max-keys=-1
+    ${AWS} s3api list-objects-v2 --bucket "${bucketName}" --prefix "${baseFileName}" --max-keys=-1
+    if [ $? -ne 255 ]; then
+        return 1
+    fi
+
+    # Server returns success with no keys when max-keys=0
+    ${AWS} s3api list-objects-v2 --bucket "${bucketName}" --prefix "${baseFileName}" --max-keys=0
+    if [ $? -ne 0 ]; then
+        return 1
+    fi
+
+    echo "Remove the object"
+    ${AWS} s3api delete-object --bucket "${bucketName}" --key "${baseFileName}"
+
+    echo "Remove the bucket"
+    ${AWS} s3api delete-bucket --bucket "${bucketName}"
+}
+
+putObject_error_02() {
+    echo "Running putObject_error_02"
+    local bucketName
+
+    bucketName=$(create_random_string)
+
+    # save md5 hash
+    hash1=$(md5sum "${DATA_DIR}/datafile-1-MB" | awk '{print $1}')
+
+    # create a bucket
+    echo "Create a bucket"
+    ${AWS} s3api create-bucket --bucket "${bucketName}"
+
+    # upload an object failure with invalid object name.
+    ${AWS} s3api put-object --body "${DATA_DIR}/datafile-1-MB" --bucket "${bucketName}" --key "/2123123\123"
+    if [ $? -ne 255 ]; then
+        return 1
+    fi
+
+    # upload an object without content-md5.
+    ${AWS} s3api put-object --body "${DATA_DIR}/datafile-1-MB" --bucket "${bucketName}" --key "datafile-1-MB" --content-md5 "invalid"
+    if [ $? -ne 255 ]; then
+        return 1
+    fi
+
+    # upload an object without content-length.
+    ${AWS} s3api put-object --body "${DATA_DIR}/datafile-1-MB" --bucket "${bucketName}" --key "datafile-1-MB" --content-length -1
+    if [ $? -ne 255 ]; then
+        return 1
+    fi
+
+    echo "Remove the bucket"
+    ${AWS} s3api delete-bucket --bucket "${bucketName}"
+}
+
 main() {
-    # Run tests
+    # Success tests
     createBucket_01
     createObject_02
+    listObjects_03
+    multipart_04
+    copy_05
+    sync_06
+
+    # Error tests
+    listObjects_error_01
+    putObject_error_02
 }
 
 _init "$@" && main
