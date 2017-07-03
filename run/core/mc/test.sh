5e84051aa5828569c68525f757eea50bb7809e7c
Nitish Tiwar <nitish@minio.io>
2017-07-02 22:32:44 -0700

    - Add mc tests - Fix top level script to not remove curl as it is used during test execution.

--- run/core/mc/test.sh
+++ run/core/mc/test.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #
 #  Mint (C) 2017 Minio, Inc.
 #
@@ -16,56 +16,214 @@
 #
 
 create_random_string() {
-    bucketName=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)
+    random_str=$(tr -dc 'a-z0-9' < /dev/urandom  | fold -w 32 | head -n 1)
+    echo "$random_str"
 }
 
+# Create a bucket and check if it exists on server
 createBuckets_01(){
-    create_random_string
-    echo "Running createBuckets_01" 
+    local bucketName
+
+    bucketName=$(create_random_string)
 
     # Make bucket
     ./mc mb "target/${bucketName}" 
 
-    echo "Testing if the bucket was created" 
     # list buckets
+    echo "Testing if the bucket was created" 
     ./mc ls target 
 
-    echo "Removing the bucket" 
     # remove bucket
+    echo "Removing the bucket" 
     ./mc rm "target/${bucketName}" 
 }
 
-createFile_02(){
-    create_random_string
-    echo "Running createFile_02" 
+# Upload an object, download it and check if it matches the uploaded object
+createObject_02(){
+    local bucketName
+
+    bucketName=$(create_random_string)
 
     # save md5 hash
-    hash1=$(md5sum "$DATA_DIR"/datafile-1-MB | awk '{print $1}')
+    hash1=$(echo $(md5sum "$DATA_DIR"/datafile-1-MB | awk '{print $1}'))
     
     # create a bucket
     echo "Creating a bucket" 
     ./mc mb "target/${bucketName}" 
 
-    # copy the file
+    # upload the file
     echo "Uploading the 1MB temp file" 
     ./mc cp "$DATA_DIR"/datafile-1-MB "target/${bucketName}" 
 
-    echo "Download the file" 
-    ./mc cp "target/${bucketName}/datafile-1-MB" /tmp/datafile-1-MB-downloaded 
-    
-    #save md5 hash of downloaded file
-    hash2=$(md5sum /tmp/datafile-1-MB-downloaded | awk '{print $1}')
-
-    echo "Testing if the downloaded file is same as local file" 
-    if [ "${hash1}" -ne "${hash2}" ]; then
+    echo "Download the file"      
+    if [ $(basename $(./mc cp --json "target/${bucketName}/datafile-1-MB" /tmp | jq -r .target)) != "datafile-1-MB" ]; then
         return 1
     fi
 
-    echo "Removing the bucket" 
+    # calculate the md5 hash of downloaded file
+    hash2=$(echo $(md5sum /tmp/datafile-1-MB | awk '{print $1}'))
+
+    echo "Testing if the downloaded file is same as local file" 
+    if [ "$hash1" != "$hash2" ]; then 
+        return 1
+    fi
+
     # remove bucket
+    echo "Removing the bucket and its contents" 
     ./mc rm --force --recursive "target/${bucketName}" 
+
+    # remove local file
+    rm /tmp/datafile-1-MB
+}
+
+# Upload an object > 64MB (MC uses multipart for more than 64MB), download it and check if it matches the uploaded object
+createObjectMultipart_03(){
+    local bucketName
+
+    bucketName=$(create_random_string)
+
+    # save md5 hash
+    hash1=$(echo $(md5sum "$DATA_DIR"/datafile-65-MB | awk '{print $1}'))
+    
+    # create a bucket
+    echo "Creating a bucket" 
+    ./mc mb "target/${bucketName}" 
+
+    # upload the file
+    echo "Uploading a 65MB temp file" 
+    ./mc cp "$DATA_DIR"/datafile-65-MB "target/${bucketName}" 
+
+    echo "Download the file" 
+    if [ $(basename $(./mc cp --json "target/${bucketName}/datafile-65-MB" /tmp | jq -r .target)) != "datafile-65-MB" ]; then
+        return 1
+    fi 
+    
+    # calculate the md5 hash of downloaded file
+    hash2=$(echo $(md5sum /tmp/datafile-65-MB | awk '{print $1}'))
+
+    echo "Testing if the downloaded file is same as local file" 
+    if [ "$hash1" != "$hash2" ]; then
+        return 1
+    fi
+
+    # remove bucket
+    echo "Removing the bucket and its contents" 
+    ./mc rm --force --recursive "target/${bucketName}" 
+
+    # remove local file
+    rm /tmp/datafile-65-MB
+}
+
+# Tests `mc mirror` by mirroring all the local content to remove bucket.
+mirrorObject_04() {
+    local bucketName
+
+    bucketName=$(create_random_string)
+
+    # create a bucket
+    echo "Creating a bucket" 
+    ./mc mb "target/${bucketName}" 
+
+    echo "Upload a set of files"
+    ./mc mirror -q "$DATA_DIR" "target/${bucketName}"   
+
+    # remove bucket
+    echo "Removing the bucket and its contents" 
+    ./mc rm --force --recursive "target/${bucketName}" 
+}
+
+# Tests for presigned URL upload success case, presigned URL
+# is correct and accessible - we calculate md5sum of
+# the object and validate it against a local files md5sum.
+presignedUploadObject_05() {
+    local bucketName
+
+    bucketName=$(create_random_string)
+    fileName="${DATA_DIR}/datafile-1-MB"
+
+    # create a bucket
+    echo "Creating a bucket" 
+    ./mc mb "target/${bucketName}" 
+
+    # save md5 hash
+    hash1=$(echo $(md5sum "$fileName" | awk '{print $1}'))
+
+    # create presigned URL object
+    echo "Create presigned file upload" 
+    url=$(./mc share --json upload "target/${bucketName}/$(basename "$fileName")" | jq -r .share)
+    
+    # upload the file
+    $(echo $url | sed "s@<FILE>@$fileName@g")
+
+    echo "Download the file"      
+    if [ $(basename $(./mc cp --json "target/${bucketName}/datafile-1-MB" /tmp | jq -r .target)) != "datafile-1-MB" ]; then
+        return 1
+    fi
+
+    # calculate the md5 hash of downloaded file
+    hash2=$(echo $(md5sum /tmp/datafile-1-MB | awk '{print $1}'))
+
+    echo "Testing if the downloaded file is same as local file" 
+    if [ "$hash1" != "$hash2" ]; then
+        return 1
+    fi
+
+    # remove bucket
+    echo "Removing the bucket and its contents" 
+    ./mc rm --force --recursive "target/${bucketName}" 
+
+    # remove local file
+    rm /tmp/datafile-1-MB
+}
+
+# Tests for presigned URL download success case, presigned URL
+# is correct and accessible - we calculate md5sum of
+# the object and validate it against a local files md5sum.
+presignedDownloadObject_06(){
+    local bucketName
+
+    bucketName=$(create_random_string)
+    fileName="${DATA_DIR}/datafile-1-MB"
+
+    # create a bucket
+    echo "Creating a bucket" 
+    ./mc mb "target/${bucketName}" 
+
+    # save md5 hash
+    hash1=$(echo $(md5sum "$fileName" | awk '{print $1}'))
+
+    # upload the file
+    echo "Uploading a 1MB temp file" 
+    ./mc cp "${fileName}" "target/${bucketName}" 
+
+    # create presigned URL download
+    echo "Create presigned file download URL" 
+    url=$(./mc share --json download "target/${bucketName}/$(basename "$fileName")" | jq -r .share)
+    
+    # download the file
+    echo "Download the file"
+    curl "$url" -o /tmp/"$(basename "$fileName")"
+
+    # calculate the md5 hash of downloaded file
+    hash2=$(echo $(md5sum /tmp/"$(basename "$fileName")" | awk '{print $1}'))
+
+    echo "Testing if the downloaded file is same as local file" 
+    if [ "$hash1" != "$hash2" ]; then
+        return 1
+    fi
+
+    # remove bucket
+    echo "Removing the bucket and its contents" 
+    ./mc rm --force --recursive "target/${bucketName}" 
+
+    # remove local file
+    rm /tmp/datafile-1-MB
 }
 
 # Run tests
 createBuckets_01
-createFile_02
+createObject_02
+createObjectMultipart_03
+mirrorObject_04
+presignedUploadObject_05
+presignedDownloadObject_06
