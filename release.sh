e542a2d1f94c655c617936e53c1fbac9f77b5421
Harshavardhana <harsha@minio.io>
2021-07-22 23:54:37 -0700

    remove useless security TLS test
    instead just use https://testssl.sh

--- release.sh
+++ release.sh
@@ -18,7 +18,6 @@
 export MINT_ROOT_DIR=${MINT_ROOT_DIR:-/mint}
 export MINT_RUN_CORE_DIR="$MINT_ROOT_DIR/run/core"
 export MINT_RUN_BUILD_DIR="$MINT_ROOT_DIR/build"
-export MINT_RUN_SECURITY_DIR="$MINT_ROOT_DIR/run/security"
 export WGET="wget --quiet --no-check-certificate"
 
 "${MINT_ROOT_DIR}"/create-data-files.sh
