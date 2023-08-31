2da412344b07863771e7e2f0aeceaedb835bf9b8
Harshavardhana <harsha@minio.io>
2023-08-31 15:30:11 -0700

    update aws/botocore dependencies

--- build/awscli/install.sh
+++ build/awscli/install.sh
@@ -27,14 +27,14 @@ TMPDIR="$(mktemp -d)"
 cd "$TMPDIR"
 
 # Download botocore and apply @y4m4's expect 100 continue fix
-(git clone --depth 1 -b 1.27.1 https://github.com/boto/botocore &&
+(git clone --depth 1 -b 1.31.37 https://github.com/boto/botocore &&
 	cd botocore &&
 	patch -p1 <"$ROOTDIR/expect-100.patch" &&
 	python3 -m pip install .) ||
 	die "Unable to install botocore.."
 
 # Download and install aws cli
-(git clone --depth 1 -b 1.25.1 https://github.com/aws/aws-cli &&
+(git clone --depth 1 -b 1.29.37 https://github.com/aws/aws-cli &&
 	cd aws-cli &&
 	python3 -m pip install .) ||
 	die "Unable to install aws-cli.."
